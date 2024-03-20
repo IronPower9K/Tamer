3aff095fbceb587c9a9214d32fbbd2d73d77a0f7
IronPower9K <114505607+IronPower9K@users.noreply.github.com>
2024-03-20 20:08:54 +0900

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -4,7 +4,7 @@ from multiprocessing import Process, Queue
 from functools import partial
 
 from torch._C import device
-from play import PyGymCallback, Player
+from play import PyGymCallback, Player, step_play, score
 
 import gym
 import pygame
@@ -19,7 +19,7 @@ from collections import deque
 import scipy
 import matplotlib.pyplot as plt
 from scipy.stats import uniform, gamma, norm, exponnorm
-import matplotlib.pyplot as plt 
+import pandas as p
 
 import torch
 import torch.nn.functional as F
@@ -27,7 +27,19 @@ import torchvision.transforms as T
 import torch.optim as optim
 from torch import nn
 
+
+
+step_c = []
+mean_credit = []
+feedb = []
+loss_value = []
+feedback_s = []
+buffer_d = []
+
 torch.manual_seed(10)
+
+env_time = dt.datetime.now()
+env_name = "ALE/Pong-v5"
 
 class Encoder(nn.Module):
     def __init__(self):
@@ -118,7 +130,7 @@ class BufferDeque():
 class NetworkController(PyGymCallback):
     '''
     state_start_time :- state start time is collected before step in before_step()
-    state_end_time :- state end time is collected after the step in after_step()
+    state_end_time :- state end time is collected  the step in after_step()
     h_time :- time at which the feedback is recorded, which comes along with the feedback in a list in after_step function
     
     questions:-
@@ -144,6 +156,10 @@ class NetworkController(PyGymCallback):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.sliding_window = deque()
         self.opt = optim.Adam(list(self.head.parameters()), lr=1e-1, weight_decay = 1e-1 )
+        self.step_counter = 0
+        self.feedback_sum = 0
+        
+        
 
     def backward(self, state, action, feedback, credit):
         state = state.to(self.device)
@@ -154,14 +170,17 @@ class NetworkController(PyGymCallback):
         self.loss_list = []
         h_hat = self.head(self.encoder(state))
         h_hat_s_a = h_hat[:, action]
+        self.q_value = h_hat_s_a
         # h_hat_s_a.requires_grad = True
         L = torch.mean(credit*(h_hat_s_a - feedback)**2)
         self.opt.zero_grad() 
         L.backward()
         self.opt.step()
         self.loss_list.append(L)
-        # print(f"feedback : {feedback}")
-        # print(f"loss: {L}, q_values: {h_hat}")
+        
+        #print(f"feedback : {feedback}")
+        #print(f"q_values: {h_hat_s_a}") #reward ??
+        #print(L) #reward sum???
 
     def before_step(self):
         self.state_start_time = time.time()
@@ -186,8 +205,9 @@ class NetworkController(PyGymCallback):
         batch=64
         loss_fn = nn.MSELoss(reduction = 'mean') 
         #only when buffer has 50 feedbacks
-       
-        if len(self.buffer) > 50:      
+        #print(len(self.buffer)) #!!!!! experience stack up to 70 sample pick random
+        
+        if len(self.buffer) > 60:      
             # Only train every certain number of steps
             if self.t % 16 == 0: 
                 #rand_batch = np.random.randint(len(self.buffer), size=batch_size) 
@@ -227,15 +247,108 @@ class NetworkController(PyGymCallback):
                         credit_for_state
                         # torch.Tensor(credit) 
                     ])
-
+            global mean_credit
+            global feedb
+            
+            if len(credit) != 0:
+                mean_credit.append(sum(credit) / len(credit))  #input reward
+                m_credit = sum(credit) / len(credit)
+            else:
+                mean_credit.append(0)
+            feedb.append(fb)
+            self.feedback_sum += fb
+            
             state, action, credit = torch.cat(state), torch.tensor(action), torch.tensor(credit)
             feedback = torch.full(credit.size(), fb)
             self.backward( state, action, feedback, credit)
+            
 
+
+            
+        else:
+            feedb.append(0)
+            mean_credit.append(0)
+            
+
+
+        
+        self.step_counter += 1  
+        global step_c
+        global feedback_s
+        global buffer_d
+        step_c.append(self.step_counter)
+        feedback_s.append(self.feedback_sum)
+        buffer_d.append(len(self.buffer))
+
+        #print(f"step: {self.step_counter} loss_value: {loss_value} mean_credit:{m_credit},feedback:{fb}")
+        #print(f"step: {self.step_counter} loss_value: {loss_value} mean_credit:{mean_credit},feedback:{feedb}")
+        #print(f"step: {self.step_counter} loss_value: {self.loss_value} mean_credit:{self.mean_credit},feedback:{self.fb}")  #this is?????????
+        if self.step_counter % 100 == 0:
+            print(f'step:{self.step_counter}, buffer: {len(self.buffer)}')
+
+
+
+        if self.step_counter % 2000 == 0:
+            #print(f"step: {step_c} loss_value: {loss_value} mean_credit:{mean_credit},feedback:{feedb}")
+            
+            fig = plt.figure(figsize=(8,8))
+            
+            df = p.DataFrame(data = list(zip(step_play, score)), columns = ["step_play", "score"])
+
+            
+
+            
+            plt.subplot(4,1,1)
+            plt.plot(step_c, mean_credit,'b-')
+            plt.xlabel("Time step")
+            plt.ylabel("mean_credit")
+            
+            plt.subplot(4,1,2)
+            plt.plot(step_c, feedback_s,'r-')
+            plt.xlabel("Time step")
+            plt.ylabel("feedback_sum")
+            
+            plt.subplot(4,1,3)
+            plt.plot(step_c, buffer_d,'y-')
+            plt.xlabel("Time step")
+            plt.ylabel("Replay_buffer")
+
+            plt.subplot(4,1,4)
+            window_size = 5000  # 이동 평균의 윈도우 크기
+            rolling_mean = df['score'].rolling(window=window_size).mean()
+            plt.plot(df['step_play'], df['score'], label='Score')
+            plt.plot(df['step_play'], rolling_mean, label=f'Rolling Mean ({window_size} steps)')
+            plt.xlabel('Step')
+            plt.ylabel('Score')
+            
+            plt.savefig(f'step_{self.step_counter}.jpg',format='jpeg')
+
+
+        
+        '''
+        loss_list mean_credit, fb, step_counter
+        
+        later view with model
+        
+        '''
+
+        #model!!!!!!!!!!!!11
+        if self.step_counter % 5000 == 0:  
+            model_save_path = f'model/{env_name}_{env_time}_model_step_{self.step_counter}.pth'  
+            torch.save(self.head.state_dict(), model_save_path)
+            print(f'Model successfully saved to {model_save_path}')
+       
+       
+       
     def after_play(self):
+        print("1111111","step counter",self.step_counter)
         plt.title('Head_Network_Error')
         plt.plot(self.loss_list)
         plt.savefig('Test_Error')
+        
+        model_save_path = 'head_network_model.pth'  # Define the file path to save the model
+        torch.save(self.head.state_dict(), model_save_path)
+        print(f'Model successfully saved to {model_save_path}')
         
 class FeedbackListener(Process):
     def __init__(self,fb_queue,video_size=(200, 100)):
@@ -265,19 +378,22 @@ class FeedbackListener(Process):
     
     def _do_pygame_events(self):
         fb, fill = 0, None
+        
         for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYUP:
                 if event.key == pygame.K_1:
                     fill = self.screen.fill((0, 255, 0))
-                    fb = 1
+                    fb = 5
                 elif event.key == pygame.K_2:
                     fill = self.screen.fill((255, 0, 0))
-                    fb = -1
+                    fb = -5
             elif event.type == pygame.VIDEORESIZE:
                 self.video_size = event.size
                 self._update_screen(fill)
             elif event.type == pygame.QUIT:
                 self.listening = False
+        
+        
         return fb, fill 
     
     def _update_screen(self, fill=None):
@@ -287,7 +403,8 @@ class FeedbackListener(Process):
         pygame.display.update(fill) 
 
 def main():
-    env = gym.make("Bowling-v0").unwrapped 
+
+    env = gym.make(env_name,difficulty=0).unwrapped
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     encoder = Encoder().to(device)
@@ -302,12 +419,12 @@ def main():
     # opt = torch.optim.Adam(head_net.parameters(), lr=1e-4, weight_decay=1e-1) 
     
     Feedback_queue = Queue()
-
+    
     listener = FeedbackListener(Feedback_queue) #pass it to listener ()
     listener.start()
     player = Player(callbacks=[NetworkController(encoder= encoder, head=head_net, queue=Feedback_queue,
-                                                    env=env, zoom=4, fps=60, human=True)]) #pass the queue
-    player.play(n_episodes = 1)
+                                                    env=env, zoom=4, fps=30, human=True)]) #pass the queue
+    player.play(n_episodes = 5)
     listener.join()
     
 
